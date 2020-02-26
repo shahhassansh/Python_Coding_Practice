868255c4bb5094d6f0aa11bbfa73e5a93c79fa91
Shah Hassan <shahhassan@live.com>
2020-02-26 05:10:50 -0500

    Modified json_intro

--- json_intro.py
+++ json_intro.py
@@ -1,11 +1,58 @@
 import json
+
+## JSON is text written with Javascript Object Notation
+
+## ------------------ ##
+## Playing with JSON
+
 person_dict = {'name': 'Bob',
 'age': 12,
 'children': None
 }
+
 person_json = json.dumps(person_dict)
-# Output: {"name": "Bob", "age": 12, "children": null}
-print(person_json)
+#print(type(person_json)) ## str
+person_final = json.loads(person_json)
+#print(type(person_final)) ## dict
+
+## --------------------------- ##
+## Playing with another JSON
+
+person_dict2 = [{'name': 'Bob',
+'age': 12,
+'children': 0
+},
+{'name': 'Shah',
+'age': 45,
+'children': 1
+},
+{'name': 'Charlie',
+'age': 65,
+'children': 3
+},
+{'name': 'Ardhy',
+'age': 36,
+'children': 1
+}]
+
+person_json2 = json.dumps(person_dict2)
+print(type(person_json2)) ## str
+person_final2 = json.loads(person_json2)
+print(type(person_final2)) ## list
+
+## Function to return age to people with only 1 child
+def age_with_one_child(x):
+    arr = []
+    for a in x:
+        print(a)
+        if a['children'] == 1:
+            arr.append(a['age'])
+    return arr
+
+print(age_with_one_child(person_final2))
+
+## ----------------------------    
+## Playing with Another JSON
 
 office_dict = { "office": 
     {"medical": [
@@ -41,9 +88,25 @@ office_dict = { "office":
       "price": 750
     }
   }
-} 
-office_json = json.dumps(office_dict)
-print(office_json[office])
-#print(office_json(parking))
+}
+## To dump into a JSON string
+office_json = json.dumps(office_dict) #str
+## To load JSON into a Dictionary
+office_final = json.loads(office_json) #dict
+
+# Function to return room number and use pair in dictionary for medical offices. 
+def room_num_use(x):
+    data = x["office"]["medical"] #list
+    out = {}
+    for a in data:
+        s = a["room-number"]
+        out[s] = a["use"]
+
+    return out
+
+print(room_num_use(office_final))
+
+
+
 
 
