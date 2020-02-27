24b32fa7627bbe44e2a01cd9241ef6de9c2408fa
Shah Hassan <shahhassan@live.com>
2020-02-27 02:00:06 -0500

    Adding inOrder Trversal Code

---
+++ inorderTraversal.py
@@ -0,0 +1,21 @@
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def inOrderTraversal(root):
+    if root != None:
+        inOrderTraversal(root.left)
+        print(root.val)
+        inOrderTraversal(root.right)
+
+
+root=TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print(inOrderTraversal(root))
+
